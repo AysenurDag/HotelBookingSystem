7ba8f71075aac41b81e99dd93feaa21960d25de1
Nigarselekk <nigarselek923@gmail.com>
2025-05-14 03:32:29 +0300

    REFACTOR REFUND PROCESS

--- nigar-payment-service/Events/BookingRefundCompletedEvent.cs
+++ nigar-payment-service/Events/BookingRefundCompletedEvent.cs
@@ -7,15 +7,9 @@ public class BookingRefundCompletedEvent
     [JsonPropertyName("bookingId")]
     public string BookingId { get; set; } = default!;
 
-    [JsonPropertyName("userId")]
-    public string UserId { get; set; } = default!;
+    [JsonPropertyName("paymentId")]
+    public long   PaymentId { get; set; } 
 
-    [JsonPropertyName("refundAmount")]
-    public decimal RefundAmount { get; set; }
-
-    [JsonPropertyName("currency")]
-    public string Currency { get; set; } = default!;
-
-    [JsonPropertyName("completedAt")]
-    public DateTime CompletedAt { get; set; }
+    [JsonPropertyName("status")]
+    public string Status    { get; set; } = default!;
 }
