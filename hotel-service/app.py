146eb92ae054349e772f43cf4bd56871e88e6173
Özge Havva Şahin <ozgehavva.02@gmail.com>
2025-05-23 23:22:54 +0300

    Consumer tamama

--- hotel-service/app.py
+++ hotel-service/app.py
@@ -1,18 +1,23 @@
 from factory import create_app
 from database import mongo
 from rabbitmq_utils import consume_messages
-from consumer import handle_message
+from consumer import handle_message, handle_cancellation_message 
 import threading
 import os
 
-def start_consumer_thread(app):
-    from flask import current_app
+def start_consumer_thread(app, queue_name, callback):
     def start():
         with app.app_context():
-            consume_messages("booking.reservation.created.queue", handle_message)
+            consume_messages(queue_name, callback)
     threading.Thread(target=start, daemon=True).start()
 
 if __name__ == '__main__':
     app = create_app(os.getenv('FLASK_ENV', 'development'))
-    start_consumer_thread(app)
+
+    # Booking reservation oluşturma mesajlarını dinle
+    start_consumer_thread(app, "booking.reservation.created.queue", handle_message)
+
+    # Reservation cancellation mesajlarını dinle
+    start_consumer_thread(app, "booking.reservation.cancelled.queue", handle_cancellation_message)
+
     app.run(host='0.0.0.0', port=5050, debug=app.config['DEBUG'])
--- hotel-service/consumer.py
+++ hotel-service/consumer.py
@@ -47,3 +47,37 @@ def handle_message(ch, method, properties, body):
         except Exception as e:
             current_app.logger.error(f"[Hotel Consumer] Failed to process message: {e}")
             ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
+
+def handle_cancellation_message(ch, method, properties, body):
+    with app.app_context():  # ✅ Flask context burada açılıyor
+        try:
+            message = json.loads(body)
+            current_app.logger.info(f"[Hotel Consumer] Cancellation message received: {message}")
+
+            booking_id = message.get("bookingId")
+            check_in = message.get("checkInDate")
+            check_out = message.get("checkOutDate")
+            room_id = message.get("roomId")
+
+            from services.room_availability_service import RoomAvailabilityService
+            from database import mongo
+
+            service = RoomAvailabilityService(mongo.db)
+
+            availability_payload = {
+                "bookingId": booking_id, 
+                "roomId": room_id,
+                "start_date": check_in,
+                "end_date": check_out,
+                "isAvailable": -1
+            }
+            
+            result = service.create_availability(availability_payload)
+            current_app.logger.info(f"[Hotel Consumer] Availability updated (booking canceled): {result}")
+
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
+        except Exception as e:
+            current_app.logger.error(f"[Hotel Consumer] Failed to process cancellation message: {e}")
+            ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
+
